cd75730c79679a5097f9d9c25b639b03c74d08ba
Jerome Campana <jeromecampana.personal@gmail.com>
2026-05-07 02:36:05 +0800

    Supplier info, partner_id filters

--- cgc_vendor_validation/models/product_supplierinfo.py
+++ cgc_vendor_validation/models/product_supplierinfo.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from lxml import etree
 
 class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
     partner_id = fields.Many2one(
         'res.partner',
-        domain="[('supplier_rank', '>', 0), ('validation_status', '=', 'validated')]"
+        domain="[('supplier_rank', '>', 0), ('validation_progress', '=', 100), ('validation_progress', '!=', False)]",
+        check_company=False
     )
